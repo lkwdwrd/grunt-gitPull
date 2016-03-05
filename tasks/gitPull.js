0cf86686d432500f9d50effdfa1c838d05b7a134
Luke Woodward <woodward.lucas@gmail.com>
2016-03-04 19:10:37 -0600

    recursively make outer directories if they don't exist

--- tasks/gitPull.js
+++ tasks/gitPull.js
@@ -9,7 +9,8 @@
 'use strict';
 
 var path  = require('path'),
-    async = require( 'async' );
+    async = require( 'async' ),
+    mkdirp = require( 'mkdirp' );
 
 
 
@@ -58,10 +59,7 @@ module.exports = function (grunt) {
       relPath.inner = relPath.outer + path.sep + dir;
       
       // validate outer directory
-      if ( ! grunt.file.isDir( relPath.outer ) ) {
-        grunt.log.warn('The directory "' + relPath + '" not found.');
-        continue;
-      }
+      mkdirp( relPath.outer );
       
       // Set up pull or clone
       if ( grunt.file.isDir( relPath.inner + path.sep  + '.git' ) ) {
